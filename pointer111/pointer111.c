c6d91f018ac5de04dcf9f5a5d783d721096ba37f
hyelin77 <nachoego4@gmail.com>
2025-05-15 09:40:25 +0900

    포인터 함수 여러개

---
+++ pointer111/pointer111.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 777;
+	call_value(i); //함수호출
+	printf("값 호출 : %d\n", i); //777
+
+	call_refer(&i); //함수호출 //i주소
+	printf("참조 호출 : %d\n", i);
+	return 0;
+}
+
+void call_value(int icopy)
+{
+	///icopy=777
+	icopy = 888;
+}
+
+void call_refer(int* ip)
+{//ip가 i주소 가리킴
+	*ip = 888; //주소가 가리키는 값
+}
